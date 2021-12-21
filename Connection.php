15904b6439dfea914113cee2f5f1ef0b972835fa
JzhTW <qaawssefu0120@gmail.com>
2021-12-21 20:35:17 +0800

    完成

---
+++ Connection.php
@@ -0,0 +1,83 @@
+<?php
+
+class Connection
+{
+
+    public PDO $pdo;
+
+    public function __construct()
+    {
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $db_name = "note_app";
+
+        try {
+            $this->pdo = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
+            // set the PDO error mode to exception
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // echo "Connected successfully";
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+
+    }
+
+    // 取得筆記
+    public function getNotes()
+    {
+        $mysqlRequest = "SELECT * FROM notes ORDER BY created_date DESC";
+        $statement = $this->pdo->prepare($mysqlRequest);
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // 新增筆記
+    public function addNote($note)
+    {
+        $mysqlRequest = "INSERT INTO notes (title, description, created_date) VALUES (:title, :description, :date)";
+        $statement = $this->pdo->prepare($mysqlRequest);
+        $statement->bindValue('title', $note['title']);
+        $statement->bindValue('description', $note['description']);
+        $statement->bindValue('date', date('Y-m-d H:i:s'));
+        return $statement->execute();
+    }
+
+    // 刪除筆記
+    public function removeNote($id)
+    {
+        $mysqlRequest = "DELETE FROM notes WHERE id = :id";
+        $statement = $this->pdo->prepare($mysqlRequest);
+        $statement->bindValue('id', $id);
+        return $statement->execute();
+    }
+
+    // 取得筆記(ID)
+    public function getNoteByID($id)
+    {
+        $mysqlRequest = "SELECT * FROM notes where id = :id";
+        $statement = $this->pdo->prepare($mysqlRequest);
+        $statement->bindValue('id', $id);
+        $statement->execute();
+        return $statement->fetch(PDO::FETCH_ASSOC) ?: [
+            'id' => '',
+            'title' => '',
+            'description' => '',
+        ];
+    }
+
+    // 更新筆記(ID,NOTE)
+    public function updateNote($id, $note)
+    {
+        $mysqlRequest = "UPDATE notes set title = :title, description = :description WHERE id = :id";
+        $statement = $this->pdo->prepare($mysqlRequest);
+        $statement->bindValue('id', $id);
+        $statement->bindValue('title', $note['title']);
+        $statement->bindValue('description', $note['description']);
+        return $statement->execute();
+    }
+
+}
+
+return new Connection();
--- index.php
+++ index.php
@@ -1,10 +1,17 @@
+<?php
+$connection = require_once("./Connection.php");
+$notes = $connection->getNotes();
+
+// echo "<pre>" . print_r($notes) . "</pre>";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>PHP 筆記本(CRUD)</title>
     <link rel="stylesheet" href="app.css">
 </head>
 <body>
@@ -15,23 +22,25 @@
         <textarea name="description" cols="30" rows="4"
                   placeholder="Note Description"></textarea>
         <button>
-          New Note
+          新增筆記
         </button>
     </form>
     <div class="notes">
+        <?php foreach($notes as $note): ?>
         <div class="note">
             <div class="title">
-                <a href="#">This is post title</a>
+                <a href="#"><?php echo $note['title'];?></a>
             </div>
             <div class="description">
-              This is description
+              <?php echo $note['description'];?>
             </div>
-            <small>2021-05-31</small>
+            <small><?php echo $note['created_date'];?></small>
             <form action="delete.php" method="post">
               <input type="hidden" name="id" value="">
             <button class="close">X</button>
             </form>
         </div>
+      <?php endforeach; ?>
     </div>
 </div>
 </body>
